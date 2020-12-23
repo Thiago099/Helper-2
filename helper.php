91be4458565d0418edd49b8f026c7d827d1205d1
Thiago099 <>
2020-12-23 15:26:53 -0300

    [feature] select and controlador

--- helper.php
+++ helper.php
@@ -571,8 +571,101 @@ class $model extends CI_Model
                 echo $ret;
               }
               ?></textarea>
+              <label>Controlador</label>
+              <textarea name="name" rows="40" cols="200" spellcheck="false"><?php
 
 
+                  $db=new sql($_GET['database']);
+                  $result=$db->query("DESC $_GET[table]");
+                  echo "\$dados_insert['$_GET[table]'] = [\n";
+                  foreach ($result as $i)
+                  {
+                    $ii=$i['Field'];
+                          if($ii=='id')         continue;
+                     else if($ii=='created_by') continue;
+                     else if($ii=='created_at') continue;
+                     else if($ii=='updated_by') continue;
+                     else if($ii=='updated_at') continue;
+                    echo ident("   '$ii' ",70)."=> \$dados->$ii,\n";
+                  }
+                  echo '];';
+                  echo "
+
+\$header = (object) \$this->input->request_headers();
+\$user = (object) \$this->jwt->decode(isset(\$header->authorization) ? \$header->authorization : \$header->Authorization, CONSUMER_KEY);
+\$dados_insert['id_usuario']=\$user->id_usuario;
+
+if ((int)\$dados->id == 0)
+{
+  \$dados_insert['$_GET[table]']['created_by'] = \$user->id_usuario;
+  \$dados_insert['$_GET[table]']['created_at'] = date('Y-m-d H:i:s', time());
+  \$result = \$this->$_GET[table]->salvar(\$dados_insert);
+}
+else
+{
+  \$dados_insert['$_GET[table]']['updated_by'] = \$user->id_usuario;
+  \$dados_insert['$_GET[table]']['updated_at'] = date('Y-m-d H:i:s', time());
+  \$result = \$this->$_GET[table]->atualizar(\$dados_insert, \$dados->id);
+}
+
+if (\$result)
+{
+  \$response['lista']  = \$this->$_GET[table]->get(\$result);
+}
+else
+{
+  \$response = [
+  'status' => 'erro',
+  'lista'  => [],
+  ];
+}
+          ";
+
+                ?></textarea>
+                <label>Select</label>
+                <textarea name="name" rows="40" cols="200" spellcheck="false"><?php
+                  function loop($database,$table,&$join,&$select)
+                  {
+                    $info=new sql("information_schema");
+                    $db=new sql($database);
+                    $fks=$info->query("SELECT K.COLUMN_NAME coluna,k.REFERENCED_TABLE_NAME tabela, k.REFERENCED_COLUMN_NAME chave
+                    FROM information_schema.TABLE_CONSTRAINTS i
+                    LEFT JOIN information_schema.KEY_COLUMN_USAGE k ON i.CONSTRAINT_NAME = k.CONSTRAINT_NAME
+                    WHERE i.CONSTRAINT_TYPE = 'FOREIGN KEY'
+                    AND i.TABLE_SCHEMA = '$database'
+                    AND i.TABLE_NAME = '$table'
+                    GROUP BY coluna;");
+                    $table=$_GET['table'];
+                    $fields="";
+                    foreach ($fks as $i) {
+                             if($i['coluna']=='created_by') continue;
+                        else if($i['coluna']=='updated_by') continue;
+                        $ct=$i['tabela'];
+                        $fields=$db->query("DESC $ct");
+                        $fk_name=str_replace("id_","",$i['coluna']);
+                        foreach ($fields as $j) {
+                          $jj=$j['Field'];
+                          if($i['chave']==$jj)continue;
+                          $select.=ident("$fk_name.$jj",70)." AS {$jj}_$fk_name,\n";
+                        }
+
+                        $join.="LEFT JOIN $ct";
+                        if($ct != $fk_name)$join.=" AS $fk_name";
+                        $join.=" ON $table.$i[coluna] = $fk_name.$i[chave]\n";
+                    }
+
+                    // foreach ($fks as $i) {
+                    //     loop($database,$i['tabela'],$join,$select);
+                    // }
+
+                  }
+                  $join='';
+                  $select=",\n";
+                  loop($_GET['database'],$_GET['table'],$join,$select);
+                  if($select==",\n")$select='';
+                  $select=substr($select, 0, -2)."\n";
+                  echo "SELECT\n$_GET[table].*{$select}FROM $_GET[table]\n$join";
+                ?></textarea>
               <?php
               endif;
                   ?>
